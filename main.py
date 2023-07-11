31d54866f0c0b98d3a0a1505ab20817eb0680bd6
actuallyasmartname <77011692+actuallyasmartname@users.noreply.github.com>
2023-07-11 15:38:57 -0400

    Asynchronous code test (speed is key)

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-import httpx as r
-import datetime
-plr = int(input("Input the UserID of the player: "))
-date = input("Input the date you want to travel back to (YYYY-MM-DD, ex. 2022-12-24): ")
-year, month, day = map(int, date.split('-'))
-date1 = datetime.date(year, month, day)
-hunnidlist = []
-count = 0
-done = False
-c = ""
-while True:
-    badgecnt = r.get(f"https://badges.roblox.com/v1/users/{plr}/badges?limit=100&cursor={c}&sortOrder=Asc", timeout=10)
-    for i in range(0, len(badgecnt.json()['data'])):
-        hunnidlist.append(badgecnt.json()['data'][i]['id'])
-    awarddates = r.get(f"https://badges.roblox.com/v1/users/{plr}/badges/awarded-dates?badgeIds={str(hunnidlist).replace('[', '').replace(']', '')}")
-    for i in range(0, len(awarddates.json()['data'])):
-        dateo = datetime.datetime.date(datetime.datetime.fromisoformat(awarddates.json()['data'][i]['awardedDate']))
-        if dateo > date1:
-            done = True
-            continue
-        else:
-            count += 1
-    if done == True or badgecnt.json()['nextPageCursor'] == None:
-        print(badgecnt.json()['previousPageCursor'])
-        print(f"Final Count: {count}")
-        break
-    print(f"Counted: {count}")
-    c = badgecnt.json()['nextPageCursor']
-    hunnidlist.clear()
-    continue
+import aiohttp, asyncio, datetime
+async def main():
+    plr = int(input("Input the UserID of the player: "))
+    date = input("Input the date you want to travel back to (YYYY-MM-DD, ex. 2022-12-24): ")
+    year, month, day = map(int, date.split('-'))
+    date1 = datetime.date(year, month, day)
+    hunnidlist = []
+    count = 0
+    done = False
+    c = ""
+    while True:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://badges.roblox.com/v1/users/{plr}/badges?limit=100&cursor={c}&sortOrder=Asc") as badgecnt:
+                for i in range(0, len((await badgecnt.json())['data'])):
+                    hunnidlist.append((await badgecnt.json())['data'][i]['id'])
+            async with session.get(f"https://badges.roblox.com/v1/users/{plr}/badges/awarded-dates?badgeIds={str(hunnidlist).replace('[', '').replace(']', '')}") as awarddates:
+                for i in range(0, len((await awarddates.json())['data'])):
+                    dateo = datetime.datetime.date(datetime.datetime.fromisoformat((await awarddates.json())['data'][i]['awardedDate']))
+                    if dateo > date1:
+                        done = True
+                        continue
+                    else:
+                        count += 1
+            if done == True or (await badgecnt.json())['nextPageCursor'] == None:
+                print((await badgecnt.json())['previousPageCursor'])
+                print(f"Final Count: {count}")
+                break
+        print(f"Counted: {count}")
+        c = (await badgecnt.json())['nextPageCursor']
+        hunnidlist.clear()
+        continue
+asyncio.run(main())
